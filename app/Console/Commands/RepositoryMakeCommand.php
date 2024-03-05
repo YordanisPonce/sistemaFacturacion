34afcdcf740ea6007d3703fb0ac57b31aeb991bc
YordanisPonce <82534079+YordanisPonce@users.noreply.github.com>
2024-03-05 11:44:08 -0500

    Update RepositoryMakeCommand.php

--- app/Console/Commands/RepositoryMakeCommand.php
+++ app/Console/Commands/RepositoryMakeCommand.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use app\Console\Commands\Traits\ServiceProviderInjector;
+use app\Console\Commands\traits\ServiceProviderInjector;
 use Illuminate\Console\Command;
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Facades\Artisan;
